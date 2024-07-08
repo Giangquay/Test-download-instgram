e4db9cdbb83e815c315aecf423296daf335118b5
Giang Minh <giangnm@ikong.vn>
2024-07-08 11:46:42 +0700

    sss

--- src/features/instagram/test.js
+++ src/features/instagram/test.js
@@ -1,16 +1,17 @@
-// const instagramGetUrl = require("instagram-url-direct");
-import qs from "qs";
-import axios from "axios";
-import * as cheerio from "cheerio";
-const instagramVid = (url_media) => {
+const axios = require("axios").default
+const qs = require("qs")
+const cheerio = require('cheerio')
+
+
+module.exports = instagramVid = (url_media) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const BASE_URL = "https://v3.saveig.app/api/ajaxSearch";
+      const BASE_URL = "https://v3.saveig.app/api/ajaxSearch"
       const params = {
         q: url_media,
         t: "media",
         lang: "en",
-      };
+      }
 
       const headers = {
         Accept: "*/*",
@@ -29,49 +30,27 @@ const instagramVid = (url_media) => {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.183",
         "X-Requested-With": "XMLHttpRequest",
-      };
+      }
 
-      const response = await axios.post(BASE_URL, qs.stringify(params), {
-        headers,
-      });
-      const responseData = response.data.data;
-      if (!responseData) reject({ results_number: 0, url_list: [] });
+      const response = await axios.post(BASE_URL, qs.stringify(params), { headers })
+      const responseData = response.data.data
+      if (!responseData) reject({ results_number: 0, url_list: [] })
 
-      const $ = cheerio.load(responseData);
-      const downloadItems = $(".download-items");
-      const result = [];
+      const $ = cheerio.load(responseData)
+      const downloadItems = $(".download-items")
+      const result = []
 
       downloadItems.each((index, element) => {
         const downloadLink = $(element)
           .find(".download-items__btn > a")
           .attr("href")
-          .replace("&dl=1", "");
-        result.push(downloadLink);
-      });
+        result.push(downloadLink)
+      })
 
-      const responseContent = await axios.get(
-        `https://i.instagram.com/api/v1/oembed/?url=${url_media}`
-      );
-      let caption = null;
-      let author = null;
-      let thumbnail = null;
-      if (responseContent) {
-        caption = responseContent.data.title;
-        author = responseContent.data.author_name;
-        thumbnail = responseContent.data.thumbnail_url;
-      }
-
-      let igresponse = {
-        results_number: result.length,
-        datas: result,
-        author,
-        caption,
-        thumbnail,
-      };
-      resolve(igresponse);
+      let igresponse = { results_number: result.length, url_list: result }
+      resolve(igresponse)
     } catch (err) {
-      reject(err);
+      reject(err)
     }
-  });
-};
-export default instagramVid;
+  })
+}
